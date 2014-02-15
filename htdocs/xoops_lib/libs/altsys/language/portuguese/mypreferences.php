47753354b674cd094d7b2dfda94b288e3fc2f097
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2014-02-15 22:59:32 +0000

    Adding AltSys 0.82 from XoopsCube

---
+++ htdocs/xoops_lib/libs/altsys/language/portuguese/mypreferences.php
@@ -0,0 +1,6 @@
+<?php
+// $Id$
+// License http://creativecommons.org/licenses/by/2.5/br/
+define("_MD_A_MYPREFERENCES_FORMTITLE","Prefer�ncias do m�dulo");
+define("_MD_A_MYPREFERENCES_UPDATED","As prefer�ncias foram atualizadas corretamente.");
+?>
